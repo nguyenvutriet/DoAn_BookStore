ff72e5af9a9f86cf200b05ac0c0116183d81bde0
nguyenvutriet <nguyenvutriet0205@gmail.com>
2025-12-07 10:56:17 +0700

    Nguyen Vu Triet 07/12/2025 Lan 2

--- src/main/java/com/example/project_bookstore/Entity/Category.java
+++ src/main/java/com/example/project_bookstore/Entity/Category.java
@@ -1,4 +1,42 @@
 package com.example.project_bookstore.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @Column(name = "categoryId", length = 10)
+    private String catagoryId;
+
+    @Column(name = "categoryName", length = 50)
+    private String categoryName;
+
+    public Category() {
+    }
+
+    public Category(String catagoryId, String categoryName) {
+        this.catagoryId = catagoryId;
+        this.categoryName = categoryName;
+    }
+
+    public String getCatagoryId() {
+        return catagoryId;
+    }
+
+    public void setCatagoryId(String catagoryId) {
+        this.catagoryId = catagoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
