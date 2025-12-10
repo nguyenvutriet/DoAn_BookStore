82703205a1d90920268d4dafe026cb93c49bc3a3
nguyenvutriet <nguyenvutriet0205@gmail.com>
2025-12-10 10:30:13 +0700

    Nguyen Vu Triet 10/12/2025 Lan 1

--- src/main/java/com/example/project_bookstore/Repository/IOrdersRepository.java
+++ src/main/java/com/example/project_bookstore/Repository/IOrdersRepository.java
@@ -2,10 +2,17 @@ package com.example.project_bookstore.Repository;
 
 import com.example.project_bookstore.Entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IOrdersRepository extends JpaRepository<Orders, String> {
     long count();
+
+    @Query("SELECT o FROM Orders o WHERE o.customer.customerId=:id")
+    public List<Orders> findByCustomerId(@Param("id") String customerId);
 }
 
